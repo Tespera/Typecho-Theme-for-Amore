889538c3ef8e8d65be9fc38d1485a6e453edfad0
Amore <tespera@foxmail.com>
2021-06-16 21:33:13 +0800

    Add files via upload

---
+++ themes/Typecho Theme for Amore/header.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * 一个简约到极致的博客
+ * @package Amore · Header
+ * @author Amore
+ * @version 2.0
+ * @link https://amore.ink/
+ */
+?>
+
+
+<header class="mod-head">
+
+    <div style="float: left;">
+        <h1 class="mod-head__title">
+            <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a>
+        </h1>
+
+        <div class="mod-head__logo">
+            <a href="<?php $this->options->siteUrl(); ?>">
+                <img class="avatar" src="<?php $this->options->themeUrl('avatar.jpg'); ?>">
+            </a>
+        </div>
+    </div>
+
+    <div style="float: right;">
+        <span>
+          <input id="switch_default" type="checkbox" class="switch_default">  
+              <label for="switch_default" class="toggleBtn"></label>
+        </span>
+    </div>
+
+
+    <!-- <dropdown>
+  <input id="toggle2" type="checkbox">
+  <label for="toggle2" class="animate" style="background: rgba(255, 255, 255, 0);"><img style="float:right;" src="<?php $this->options->themeUrl('menu.svg'); ?>" alt="" width="26" height="26"></label>
+
+<ul class="animate">
+<div class="animate"></div></br></br>
+
+
+                    <?php $this->widget('Widget_Contents_Page_List')->to($pages); ?>
+
+                    <?php while ($pages->next()): ?>
+
+                    <li class="animate">
+
+                    <a <?php if ($this->is('page', $pages->slug)): ?>
+                      class="menu-item" <?php endif; ?> 
+                      href="<?php $pages->permalink(); ?>" 
+                      title="<?php $pages->title(); ?>">
+
+                      <?php $pages->title(); ?>
+                        
+                    </a> </li>
+
+                    <?php endwhile; ?>
+  </ul>
+  
+</dropdown>  -->
+    <!--    </div>-->
+</header>
+
+
+<a href="#" id="back-to-top" style="display: inline;"> ^ </a>
---
+++ themes/Typecho Theme for Amore/tags.php
@@ -0,0 +1,87 @@
+﻿<?php
+/**
+ * 标签
+ *
+ * @package custom
+ *
+ */
+?>
+
+
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<?php $this->need('head.php'); ?>
+
+<?php $this->need('header.php'); ?>
+
+</br></br></br>
+
+
+
+<article class="mod-post mod-post__type-post">
+
+	<header>
+
+		<h1 class="mod-post__title">
+
+			<a href="<?php $this->permalink() ?>"><?php $this->title() ?></a>
+
+		</h1>
+
+	</header>
+
+	<div class="mod-post__entry wzulli">
+
+    	        <div class="tags">
+
+        <?php $this->widget('Widget_Metas_Tag_Cloud', 'sort=mid&ignoreZeroCount=1&desc=0&limit=999999')->to($tags); ?>
+
+        <?php if($tags->have()): ?>
+
+        <ul class="tags-list">
+
+        <?php while ($tags->next()): ?>
+
+<!--         <li>
+
+            <a href="<?php $tags->permalink(); ?>" rel="tag" class="size-<?php $tags->split(5, 10, 20, 30,); ?>" title="<?php $tags->count(); ?> 个话题">
+           
+                <?php $tags->name(); ?>
+                
+            </a>
+
+        </li> -->
+
+        <li>
+
+            <a href="<?php $tags->permalink(); ?>" rel="tag" class="size-<?php $tags->split(5, 10, 20, 30,); ?>" title="<?php $tags->count(); ?> 个话题">
+           
+                # <?php $tags->name(); ?> <span>(<?php $tags->count(); ?>)</span>
+                
+            </a>
+
+        </li>
+
+        <?php endwhile; ?>
+
+        <?php else: ?>
+
+        <li>
+
+        <?php _e('没有任何标签'); ?>
+            
+        </li>
+
+        </div>
+
+        <?php endif; ?>
+
+        </ul>
+
+
+	</div>
+	
+	
+</article>
+
+	<?php $this->need('footer.php'); ?>
